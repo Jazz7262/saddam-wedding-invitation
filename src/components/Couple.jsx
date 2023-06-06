24f183eebf4862e1775a65c7cb1c280f8d6df08b
ziauddinziya07 <ziauddinziya07@gmail.com>
2023-06-06 09:45:17 +0530

    third commit

--- src/components/Couple.jsx
+++ src/components/Couple.jsx
@@ -57,7 +57,7 @@ function Couple() {
                                     <div className="img-wrapper">
                                         <img
                                             className="couple-img"
-                                            src="../assets/images/groom.png"
+                                            src="../assets/images/groom1.jpeg"
                                             alt="couple_img"
                                         />
                                     </div>
@@ -66,7 +66,7 @@ function Couple() {
                                     <div className="img-wrapper">
                                         <img
                                             className="couple-img"
-                                            src="../assets/images/groom.png"
+                                            src="../assets/images/groom2.jpeg"
                                             alt="couple_img"
                                         />
                                     </div>
@@ -75,7 +75,7 @@ function Couple() {
                                     <div className="img-wrapper">
                                         <img
                                             className="couple-img"
-                                            src="../assets/images/groom.png"
+                                            src="../assets/images/groom3.png"
                                             alt="couple_img"
                                         />
                                     </div>
